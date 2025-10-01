79502520979232beffed9a718bd987d8a24b6bd7
Dev-Arnold <igbinakearnold@gmail.com>
2025-10-01 22:35:08 +0100

    Time again

--- server.js
+++ server.js
@@ -174,8 +174,8 @@ cron.schedule('57 8 * * 1-6', async () => {
   await createAuctionSession(startTime, endTime);
 });
 
-cron.schedule('30 18 * * 1-6', async () => {
-  console.log('Starting evening auction (6:30 PM WAT)');
+cron.schedule('38 22 * * 1-6', async () => {
+  console.log('Starting evening auction (10:38 PM WAT)');
   const startTime = new Date();
   const endTime = new Date(startTime.getTime() + 60 * 60 * 1000); // 60 minutes
   await createAuctionSession(startTime, endTime);
@@ -190,7 +190,7 @@ cron.schedule('30 18 * * 0', async () => {
 });
 
 // End auctions
-cron.schedule('0 10 * * 1-6', endAuctionSession); // End morning auction at 10:00 AM
+cron.schedule('43 22 * * 1-6', endAuctionSession); // End morning auction at 10:43 PM
 cron.schedule('30 19 * * *', endAuctionSession); // End evening/Sunday auction at 7:30 PM
 
 // Handle expired reservations every 5 minutes
